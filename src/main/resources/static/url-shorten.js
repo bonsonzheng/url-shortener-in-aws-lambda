f28fbc6a60181a32afd5d227ee631bb53a299ead
zhengbangsheng <bonsonzheng@gmail.com>
2020-08-08 18:32:59 +0800

    change to new api url

--- src/main/resources/static/url-shorten.js
+++ src/main/resources/static/url-shorten.js
@@ -2,7 +2,7 @@ function post_url() {
 
     let long_url = document.getElementById("url").value
     let result = document.getElementById("result")
-    let url = "https://fs8k95s4t3.execute-api.ap-northeast-2.amazonaws.com/v1/url-shorten";
+    let url = "https://l0g2ze9dq6.execute-api.ap-northeast-2.amazonaws.com/v1/url-shorten";
 
     var data = JSON.stringify({"longUrl": long_url});
 
